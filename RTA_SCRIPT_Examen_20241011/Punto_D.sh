3852938727c2e48856a780acb421e9c2d6edd8b5
damiancubilla27 <damiancubilla27@gmail.com>
2024-10-11 23:05:12 +0000

    feat(estructura asimetrica): Nuevas carpetas y directorio

--- RTA_SCRIPT_Examen_20241011/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_D.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 
+echo
+echo "Creo estructura: "
 mkdir -p Estructura_Asimetrica/{{correo,clientes}/cartas_{1..100},correo/carteros_{1..10}}
+echo
+echo "Muestro tree: "
+tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
